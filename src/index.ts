42802c5dcd876525c1d1de5a1ba3c9cd43a0569f
Slayerus <filipmarczaknowytelefon@gmail.com>
2021-08-27 23:03:50 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,20 @@ export class Logger {
       LogType.Error
     );
   }
+  private static tracec(error: string) {
+    const orig = Error.prepareStackTrace;
+    Error.prepareStackTrace = (_, stack) => stack;
+    const err = new Error();
+    Error.captureStackTrace(err, arguments.callee);
+    const calle = err.stack[2];
+    Error.prepareStackTrace = orig;
+    this.Log(
+      `${(calle as any).getFileName()}:${(
+        calle as any
+      ).getLineNumber()}: ${error}\n`,
+      LogType.CriticalError
+    );
+  }
 
   static Log(msg: string, type?: LogType): void {
     console.log(LogAnsi[type] + msg + LogAnsi[4]);
@@ -42,7 +56,16 @@ export class Logger {
   static Debug(msg: string) {
     this.Log(msg, LogType.Debug);
   }
-  static Err(msg: string) {
+  static Error(msg: string) {
     this.trace(msg);
+  }
+  static E(msg: string) {
+    this.Error(msg);
+  }
+  static Critical(msg: string) {
+    this.tracec(msg);
+  }
+  static C(msg: string) {
+    this.Critical(msg);
   }
 }
